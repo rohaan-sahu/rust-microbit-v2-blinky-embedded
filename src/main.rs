51c5749333aea1d9be988bf5ef79f4d3ae203afa
rohaan-sahu <rbbsr8292@gmail.com>
2026-06-26 15:37:13 +0000

    column1 snake with HAL

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::asm::nop;
-use embedded_hal::digital::OutputPin;
-use embedded_hal::digital::PinState;
-use nrf52833_hal as hal;
-use nrf52833_hal::gpio::Level;
+use panic_halt as _;
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::Timer, display::blocking::Display};
 
-#[panic_handler] // panicking behavior
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {
-        cortex_m::asm::bkpt();
-    }
-}
 
-#[cortex_m_rt::entry]
+#[entry]
 fn main() -> ! {
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
+  
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
-    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
-
-    let mut is_on: bool = false;
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+    let mut row2 = board.display_pins.row2;
+    let mut row3 = board.display_pins.row3;
+    let mut row4 = board.display_pins.row4;
+    let mut row5 = board.display_pins.row5;
 
     loop {
-        let _ = row1.set_state(PinState::from(is_on));
+        let _ = row1.set_low();
+        timer.delay_ms(200);
 
-        for _ in 1..100_000 {
-            nop();
-        }
+        let _ = row2.set_low();
+        timer.delay_ms(200);
 
-        is_on = !is_on;
+        let _ = row3.set_low();
+        timer.delay_ms(200);
+
+        let _ = row1.set_high();
+        timer.delay_ms(200);
+
+        let _ = row4.set_low();
+        timer.delay_ms(200);
+
+        let _ = row2.set_high();
+        timer.delay_ms(200);
+
+        let _ = row5.set_low();
+        timer.delay_ms(200);
+     
+        let _ = row3.set_high();
+        timer.delay_ms(200);
+
+        let _ = row4.set_high();
+        timer.delay_ms(200);
+
+        let _ = row5.set_high();
+        timer.delay_ms(200);
+    
     }
 }
