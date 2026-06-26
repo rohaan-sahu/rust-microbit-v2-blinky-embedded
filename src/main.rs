fe6357e101a692b06b631184be7249449f81249d
rohaan-sahu <rbbsr8292@gmail.com>
2026-06-26 14:34:20 +0000

    pac pointer manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
 #![no_std]
 #![no_main]
 
-use core::ptr::write_volatile;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
+use nrf52833_pac::Peripherals;
+
 
 #[entry]
 fn main() -> ! {
-    const GPIO0_PINCNF21_ROW1_ADDR: *mut u32 = 0x5000_0754 as *mut u32;
-    const GPIO0_PINCNF28_COL1_ADDR: *mut u32 = 0x5000_0770 as *mut u32;
-    const DIR_OUTPUT_POSITION: u32 = 0;
-    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POSITION ;
+    let p = Peripherals::take().unwrap();
+    p.P0.pin_cnf[22].write(|w| w.dir().output());
+    p.P0.pin_cnf[30].write(|w| w.dir().output());
 
-    unsafe {
-        //*GPIO0_PINCNF21_ROW1_ADDR = PINCNF_DRIVE_LED;
-        //*GPIO0_PINCNF21_COL1_ADDR = PINCNF_DRIVE_LED;
-        write_volatile(GPIO0_PINCNF21_ROW1_ADDR, PINCNF_DRIVE_LED);
-        write_volatile(GPIO0_PINCNF28_COL1_ADDR, PINCNF_DRIVE_LED);
-    }
-
-    const GPIO0_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
-    const GPIO0_OUT_ROW1_POS: u32 = 21;
-    let mut is_on: bool = false; 
+    let mut is_on: bool = false;
 
     loop {
-        unsafe {
-            write_volatile(GPIO0_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
-        }
+        p.P0.out.write(|w| w.pin22().bit(is_on));
 
-        for _ in 1..400_000 {
+        for _ in 1..200_000 {
             nop();
         }
 
